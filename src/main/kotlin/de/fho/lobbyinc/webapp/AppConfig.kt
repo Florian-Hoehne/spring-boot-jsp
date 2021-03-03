aafb87910fae737671de451c3585be54936da214
Florian Höhne <florian.hoehne@pdv.de>
2021-03-03 13:23:38 +0100

    serve jsp from bootable jar

--- src/main/kotlin/de/fho/lobbyinc/webapp/AppConfig.kt
+++ src/main/kotlin/de/fho/lobbyinc/webapp/AppConfig.kt
@@ -1,5 +1,7 @@
 package de.fho.lobbyinc.webapp
 
+import org.apache.catalina.Context
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
@@ -7,6 +9,16 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 import org.springframework.web.servlet.view.InternalResourceViewResolver
 import org.springframework.web.servlet.view.JstlView
+import org.springframework.boot.web.embedded.tomcat.TomcatContextCustomizer
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory
+
+import org.springframework.boot.web.server.WebServerFactoryCustomizer
+
+
+
+
+
+
 
 @Configuration
 @ComponentScan("de.fho.lobbyinc.webapp")
@@ -18,12 +30,16 @@ class AppConfig : WebMvcConfigurer
             .setCachePeriod(0)
     }
 
-    //@Bean // filesViewController: UrlFilenameViewController
-//    fun mapping(): SimpleUrlHandlerMapping {
-//        val mapper = SimpleUrlHandlerMapping()
-//
-//        return mapper
-//    }
+    @Bean
+    @ConditionalOnProperty(name = ["server.static"], matchIfMissing = true)
+    fun sessionManagerCustomizer(): WebServerFactoryCustomizer<TomcatServletWebServerFactory>? {
+        return WebServerFactoryCustomizer { server: TomcatServletWebServerFactory ->
+            server.addContextCustomizers(TomcatContextCustomizer { context: Context ->
+                context.sessionTimeout = 24 * 60
+                context.addLifecycleListener(TomcatStaticResourceLifecycle(context))
+            })
+        }
+    }
 
     @Bean
     fun jspViewResolver(): InternalResourceViewResolver
